c9356ae363fa692bc89b9060fa75870f0cdc6b87
Vikentii Ekgart <Vikentii_Ekgart@epam.com>
2019-05-07 15:33:55 +0400

    added gmt+4 time zone for Saratov and refactoring

--- skills/pairs/index.js
+++ skills/pairs/index.js
@@ -3,14 +3,16 @@ const chislOrZnam = require('./logic');
 const weekPair = {
     callName: 'какая неделя|неделя какая',
     action: (bot, message) => {
-        let parity = 'сейчас ' + chislOrZnam();
+        let d = new Date();
+        d.setHours(d.getHours() + 1); //for GMT+4
+        
+        let parity = 'сейчас ' + chislOrZnam(d);
+        
         if (/завтра/i.test(message.text)) {
-            let d = new Date();
             d.setDate(d.getDate() + 1);
             parity = 'завтра ' + chislOrZnam(d);
         }
         if (/следующая/i.test(message.text)) {
-            let d = new Date();
             d.setDate(d.getDate() + 7);
             parity = 'следующая неделя ' + chislOrZnam(d);
         }
